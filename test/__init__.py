77569dd977b961b2a799c3b46505f8316c2cbb72
DrReetuSharma <162538254+DrReetuSharma@users.noreply.github.com>
2024-05-27 18:19:31 +0530

    Update __init__.py

--- test/__init__.py
+++ test/__init__.py
@@ -11,3 +11,6 @@ os.environ['TEST_ENV'] = 'testing'
 
 # Import common utilities
 from .utils import setup_test_database
+
+#  the __init__.py file sets up logging, configures environment variables, and imports a setup_test_database function from a utils.py module within the tests/ package. 
+#These setup tasks ensure that the test environment is properly configured before running any tests.
